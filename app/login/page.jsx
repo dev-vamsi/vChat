d8b868db8dd8fc82ba10110c7e1d4ddb8f658244
Vamsi Krishna <iam.vamsitupakula@gmail.com>
2024-09-16 17:55:07 +0530

    Initial commit

---
+++ app/login/page.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+    return <h1 className="text-center text-4xl">Login</h1>;
+};
+
+export default Login;
---
+++ app/signup/page.jsx
@@ -0,0 +1,5 @@
+const Signup = () => {
+    return <h1 className="text-center text-4xl">Signup</h1>;
+};
+
+export default Signup;
