96800b3ca620021c1c63556ceef45f98489dd011
Vamsi Krishna <iam.vamsitupakula@gmail.com>
2024-09-17 21:20:35 +0530

    Login page ui added

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`antialiased`}>
-                <div className="container mx-auto py-4">{children}</div>
+            <head>
+                <script
+                    src="https://kit.fontawesome.com/4fe38eed67.js"
+                    crossOrigin="anonymous"
+                ></script>
+            </head>
+            <body className={`antialiased bg-[#fdf9f7]`}>
+                <div className="container mx-auto py-4 px-2 flex_row_center min-h-[100vh]">
+                    {children}
+                </div>
             </body>
         </html>
     );
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,36 @@
 const Login = () => {
-    return <h1 className="text-center text-4xl">Login</h1>;
+    return (
+        <div className="login flex_col_center text-justify gap-3 rounded-2xl shadow-lg px-6 py-8 bg-white">
+            <div className="flex_col_center gap-5 min-w-full md:min-w-[600px]">
+                <h1 className="text_heading text-center my-2">
+                    Welcome to <span className="text-[#427DF6]">vChat</span>,
+                    Please Login
+                </h1>
+                <input
+                    type="text"
+                    className="text_paragraph p-2 rounded-md border-2 border-[#427DF6] outline-none w-full"
+                    placeholder="Enter your email..."
+                />
+                <input
+                    type="password"
+                    className="text_paragraph p-2 rounded-md border-2 border-[#427DF6] outline-none w-full"
+                    placeholder="Enter your password..."
+                />
+                <button className="bg-[#185519] rounded-lg py-2 px-5 text-white text_paragraph">
+                    Login
+                </button>
+            </div>
+            <div className="w-full h-[1px] bg-black mb-5" />
+            <div className="flex_row_center gap-5 text-[#427DF6] text-lg text_paragraph">
+                <button className="bg-gray-200 rounded-lg w-[45px] h-[45px] shadow-lg hover:scale-110 duration-150">
+                    <i className="fa-brands fa-google mx-2"></i>
+                </button>
+                <button className="bg-gray-200 rounded-lg w-[45px] h-[45px] shadow-lg hover:scale-110 duration-150">
+                    <i className="fa-brands fa-github"></i>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
